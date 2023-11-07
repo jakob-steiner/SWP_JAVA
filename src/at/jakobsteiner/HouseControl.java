b5e878cc86a5393123cb9ce6d46197d990221a6d
Jakob Steiner <jakob.steiner05@outlook.com>
2023-11-07 15:18:05 +0100

    add headController

--- src/at/jakobsteiner/HouseControl.java
+++ src/at/jakobsteiner/HouseControl.java
@@ -10,11 +10,13 @@ public class HouseControl {
         AwningController ac2 = new AwningController();
         HeatController hc1 = new HeatController();
         HeatController hc2 = new HeatController();
+        HeatController hc3 = new HeatController();
 
         Thermomether thermomether = new Thermomether();
 
         thermomether.addObserver(ac1);
         thermomether.addObserver(hc2);
+        thermomether.addObserver(hc3);
 
         for (int i = 0; i < 20; i++) {
             thermomether.setTemperature();
